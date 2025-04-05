67b56cab72d3e6c81019f73c256d828c11d59691
Aruj Garg <arujgarg04@gmail.com>
2025-04-06 03:27:15 +0530

    deploy fixes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import { WebSocketServer, WebSocket } from "ws";
-
-const wss = new WebSocketServer({port: 8080});
+const port = process.env.PORT ? parseInt(process.env.PORT) : 8080
+const wss = new WebSocketServer({port});
 
 interface User {
     socket: WebSocket
